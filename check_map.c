0f2f5333afccb34ff48ab364579523b56e043b52
Ayman Elasefar <ayelasef@e2r8p7.1337.ma>
2025-02-02 14:24:15 +0100

    check all case to be map valid.

--- check_map.c
+++ check_map.c
@@ -16,7 +16,7 @@ int	chaeck_rectangular(char **map)
 {
 	size_t	len;
 	size_t	len_last;
-	size_t len_y;
+	size_t	len_y;
 	int		y;
 	int		i;
 	char	*last_line;
@@ -37,7 +37,7 @@ int	chaeck_rectangular(char **map)
 			return (0);
 		i++;
 	}
-	return(1);
+	return (1);
 }
 
 int	check_one_component(char **map, char c)
@@ -64,14 +64,15 @@ int	check_one_component(char **map, char c)
 
 int	check_all_components(char **map)
 {
-	if (check_one_component(map, 'E') != 1 || check_one_component(map, 'P') != 1 || check_one_component(map, 'C') == 0)
+	if (check_one_component(map, 'E') != 1 || check_one_component(map, 'P') != 1
+		|| check_one_component(map, 'C') == 0)
 		return (0);
 	return (1);
 }
 
 int	check_walls(char **map)
 {
-	int	x, (y), (last_of_line);
+	int x, (y), (last_of_line);
 	last_of_line = 0;
 	y = 0;
 	while (map[y])
@@ -82,7 +83,7 @@ int	check_walls(char **map)
 			return (0);
 		while (map[y][x] != '\n')
 		{
-			if (map[0][x] != '1' )
+			if (map[0][x] != '1')
 				return (0);
 			x++;
 		}
@@ -90,7 +91,7 @@ int	check_walls(char **map)
 	}
 	y--;
 	x = 0;
-	while(map[y][x] != '\n')
+	while (map[y][x] != '\n')
 	{
 		if (map[y][x] != '1')
 			return (0);
@@ -99,4 +100,42 @@ int	check_walls(char **map)
 	return (1);
 }
 
+int	is_map_valid(char **map, int width, int height)
+{
+	int		x;
+	int		y;
+	int		player_x;
+	int		player_y;
+	int		total_coins;
+	int		collected_coins;
+	int		exit_found;
+	char	**map_copy;
 
+	player_x = -1;
+	player_y = -1;
+	total_coins = 0;
+	collected_coins = 0;
+	exit_found = 0;
+	y = 0;
+	while (y < height)
+	{
+		x = 0;
+		while (x < width)
+		{
+			if (map[y][x] == 'P')
+			{
+				player_x = x;
+				player_y = y;
+			}
+			if (map[y][x] == 'C')
+				total_coins++;
+			x++;
+		}
+		y++;
+	}
+	if (player_x == -1 || player_y == -1)
+		return (0);
+	map_copy = ft_map_copy(map, height);
+	flood_fill(map_copy, player_x, player_y, &exit_found, &collected_coins);
+	return (collected_coins == total_coins && exit_found);
+}
