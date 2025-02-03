a065066fbd59867169ed25a4af4e5ff6faa038ea
aelasefa <aymanelasefar1337@gmail.com>
2025-02-03 22:48:55 +0100

    fix memory leaks and protect path of images

--- map_to_image.c
+++ map_to_image.c
@@ -6,79 +6,74 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 09:26:18 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/02/01 19:04:54 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/02/03 20:52:02 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	*check_image(t_game *game, char *path, int *width, int *height)
+{
+	void	*img;
+
+	img = mlx_xpm_file_to_image(game->mlx_connection, path, width, height);
+	if (!img)
+	{
+		ft_printf("Error\ncould not load image: %s\n", path);
+		free_game_resources(game);
+		exit(1);
+	}
+	return (img);
+}
+
 void	file_to_image_for_coins(t_game *game, int image_width, int image_height)
 {
-	game->coin_frames[0] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[0] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin1.xpm", &image_width,
 			&image_height);
-	game->coin_frames[1] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[1] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin2.xpm", &image_width,
 			&image_height);
-	game->coin_frames[2] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[2] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin3.xpm", &image_width,
 			&image_height);
-	game->coin_frames[3] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[3] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin4.xpm", &image_width,
 			&image_height);
-	game->coin_frames[4] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[4] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin5.xpm", &image_width,
 			&image_height);
-	game->coin_frames[5] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[5] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin6.xpm", &image_width,
 			&image_height);
-	game->coin_frames[6] = mlx_xpm_file_to_image(game->mlx_connection,
+	game->coin_frames[6] = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/coin7.xpm", &image_width,
 			&image_height);
 }
 
 void	file_to_image(t_game *game, int image_width, int image_height)
 {
-	game->image_wall = mlx_xpm_file_to_image(game->mlx_connection,
+	game->image_wall = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/wall.xpm", &image_width,
 			&image_height);
-	game->image_player = mlx_xpm_file_to_image(game->mlx_connection,
+	game->image_player = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/player1.xpm", &image_width,
 			&image_height);
-	game->image_emty_space = mlx_xpm_file_to_image(game->mlx_connection,
+	game->image_emty_space = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/emty_space.xpm",
 			&image_width, &image_height);
-	game->image_exit = mlx_xpm_file_to_image(game->mlx_connection,
+	game->image_exit = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/exit1.xpm", &image_width,
 			&image_height);
-	game->image_enemy = mlx_xpm_file_to_image(game->mlx_connection,
+	game->image_enemy = check_image(game,
 			"/home/ayelasef/Desktop/so_long/assets/enmy.xpm", &image_width,
 			&image_height);
 	file_to_image_for_coins(game, image_width, image_height);
 }
 
-void	draw_player(t_game *game, int x, int y)
-{
-	game->player_x = x;
-	game->player_y = y;
-	mlx_put_image_to_window(game->mlx_connection, game->mlx_window,
-		game->image_player, x * 64, y * 64);
-}
-
-void	draw_exit(t_game *game, int x, int y)
-{
-	game->map_x = x;
-	game->map_y = y;
-	mlx_put_image_to_window(game->mlx_connection, game->mlx_window,
-		game->image_exit, x * 64, y * 64);
-}
-
 void	ft_change_map_to_images(char **map, t_game *game)
 {
-	int (x), (y), (image_width), (image_height);
-	image_width = 0;
-	image_height = 0;
-	file_to_image(game, image_width, image_height);
+	int (x), (y);
 	y = 0;
 	while (map[y])
 	{
