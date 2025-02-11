9554f44551c47ffbcd23c3f94b8a47784d8c3a84
aelasefa <aymanelasefar1337@gmail.com>
2025-02-11 10:06:57 +0100

    check validete map

--- flood_fill.c
+++ flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 09:53:46 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/02/02 10:09:30 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/02/11 09:42:51 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,17 @@ char	**ft_map_copy(t_game *game, char **map, int y)
 
 void	flood_fill(t_game *game, int x, int y)
 {
-	if (!game->map_copy || !game->map_copy[y] || !game->map_copy[y][x]
-		|| game->map_copy[y][x] == '1' || game->map_copy[y][x] == 'V'
-		|| game->map[y][x] == 'X')
+	if (x < 0 || y < 0 || !game->map_copy || !game->map_copy[y]
+		|| !game->map_copy[y][x] || game->map_copy[y][x] == '1'
+		|| game->map_copy[y][x] == 'V')
+	{
+		game->map_flag = 4;
 		return ;
+	}
 	if (game->map_copy[y][x] == 'E')
 		game->exit_found = 1;
-	if (game->map_copy[y][x] == 'E')
-		game->enemy_found = 1;
 	if (game->map_copy[y][x] == 'C')
-		game->collected_coins++;
+		game->collected_coins_f++;
 	game->map_copy[y][x] = 'V';
 	flood_fill(game, x - 1, y);
 	flood_fill(game, x + 1, y);
@@ -77,7 +78,10 @@ int	is_map_valid(t_game *game, char **map, int width, int height)
 	game->total_coins_f = 0;
 	check_player_and_coin(game, width, height);
 	if (game->player_x_f == -1 || game->player_y_f == -1)
+	{
+		game->map_flag = 4;
 		return (0);
+	}
 	game->collected_coins = 0;
 	game->exit_found = 0;
 	game->enemy_found = 0;
@@ -85,6 +89,5 @@ int	is_map_valid(t_game *game, char **map, int width, int height)
 	flood_fill(game, game->player_x_f, game->player_y_f);
 	free_map(game->map_copy);
 	game->map_copy = NULL;
-	return (game->collected_coins == game->total_coins_f && game->exit_found
-		&& game->enemy_found);
+	return (game->collected_coins_f == game->total_coins_f && game->exit_found);
 }
