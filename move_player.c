80551de3e5890d295219737edc8352e190623ea9
aelasefa <aymanelasefar1337@gmail.com>
2025-02-11 10:03:18 +0100

    move player in game

--- move_player.c
+++ move_player.c
@@ -6,7 +6,7 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 12:31:23 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/02/03 22:25:33 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/02/11 08:34:52 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,13 +67,12 @@ int	key_hook(int key, void *game_ptr, char **map)
 	{
 		ft_collect_coins(game, &new_x, &new_y);
 		if (game->map[new_y][new_x] == 'X')
-		{
-			ft_printf("You lose\n");
-			free_game_resources(game);
-			exit(1);
-		}
+			ft_lose(game);
 		if (game->map[game->player_y][game->player_x] != 'E')
+		{
 			game->map[game->player_y][game->player_x] = '0';
+			game->map[new_y][new_x] = 'P';
+		}
 		game->player_x = new_x;
 		game->player_y = new_y;
 		if (check_key(key))
@@ -91,7 +90,7 @@ void	render_game(t_game *game)
 	mlx_put_image_to_window(game->mlx_connection, game->mlx_window,
 		game->image_player, game->player_x * 64, game->player_y * 64);
 	tmp = ft_itoa(game->moves);
-	mlx_string_put(game->mlx_connection, game->mlx_window, game->map_height / 2
-		* 10, game->map_width / 2 * 10, 0xFFFFFFF, tmp);
+	mlx_string_put(game->mlx_connection, game->mlx_window, 1 * 64, 1 * 64,
+		0xFFFFFFF, tmp);
 	free(tmp);
 }
